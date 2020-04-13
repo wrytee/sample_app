45789af8e0460791e8d717deaf09e44e127c5572
Matthew Wright <matthew.wright5@gmail.com>
2020-04-13 06:21:02 +0000

    implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
       redirect_to user
@@ -17,6 +17,8 @@ class SessionsController < ApplicationController
   end
   
   def destroy
+    log_out
+    redirect_to root_url
   end
   
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,63 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "login with valid email/invalid password" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: {  session: {  email:      @user.email,
+                                            password:   "invalid" } }
+    assert_not is_logged_in?
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+  
+  test "login with valid information followed by logout" do
+    # Visit the login path.
+    get login_path
+    
+    # Post valid information to the session path.
+    post login_path, params: { session: { email:    @user.email,
+                                          password:  'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    
+    # Verfify that the login link disappears.
+    assert_select "a[href=?]", login_path, count: 0
+    
+    # Verfiy that a logout link appears.
+    assert_select "a[href=?]", logout_path
+    
+    # Verfiy that a profile link appears.
+    assert_select "a[href=?]", user_path(@user)
+    
+    # Log the user out.
+    delete logout_path
+    
+    # Confirm the user is not logged in.
+    assert_not is_logged_in?
+    
+    # Verify the user was returned to the home page.
+    assert_redirected_to root_url
+    follow_redirect!
+    
+    # Verify a log-in link appears.
+    assert_select "a[href=?]", login_path
+    
+    # Verify the logout link does not appear.
+    assert_select "a[href=?]", logout_path,       count: 0
+    
+    # Verify that the users link does not appear.
+    assert_select "a[href=?]", user_path(@user),  count: 0
+  end
 
   test "login with invalid information" do
     # Visit the login path.
