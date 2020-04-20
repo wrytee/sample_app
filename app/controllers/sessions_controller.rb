34e5d56fbf2874349a87e1ac8f35391a351afcff
Matthew Wright <matthew.wright5@gmail.com>
2020-04-20 07:25:12 +0000

    debug mailer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
-      log_in user
-      # If the 'remember me' checkbox is selected, remember the user, otherwise forget them.
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        # Log the user in and redirect to the user's show page.
+        log_in user
+        # If the 'remember me' checkbox is selected, remember the user, otherwise forget them.
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = "Account not activated."
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination'
